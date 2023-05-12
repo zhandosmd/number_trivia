6b276fd2a1554b25e4d8921ab0e2b883c3e19478
Zhandos <zhandosmedetov@gmail.com>
2023-05-13 01:25:40 +0600

    l10 bloc scaffolding & input converter

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -0,0 +1,30 @@
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaState extends Equatable {
+  const NumberTriviaState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia numberTrivia;
+
+  const Loaded({required this.numberTrivia});
+
+  @override
+  List<Object> get props => [numberTrivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  const Error({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
