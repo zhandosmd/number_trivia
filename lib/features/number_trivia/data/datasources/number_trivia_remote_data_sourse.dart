3b412dd3bfcabd0d06da83daaf0aa2d01c9fdab9
Zhandos <zhandosmedetov@gmail.com>
2023-05-18 18:02:51 +0600

    l13 - Dependency Injection

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_sourse.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_sourse.dart
@@ -5,7 +5,7 @@ import 'package:number_trivia/core/error/exception.dart';
 import '../models/number_trivia_model.dart';
 import 'package:http/http.dart' as http;
 
-abstract class NumberTriviaRemoteDataSource{
+abstract class NumberTriviaRemoteDataSource {
   /// Calls the http://numbersapi.com/{number} endpoint.
   ///
   /// Throws a [ServerException] for all error codes.
@@ -17,21 +17,22 @@ abstract class NumberTriviaRemoteDataSource{
   Future<NumberTriviaModel> getRandomNumberTrivia();
 }
 
-class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource{
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
   final http.Client client;
 
   NumberTriviaRemoteDataSourceImpl({required this.client});
 
   @override
-  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) => _getTriviaFromUrl('http://numbersapi.com/$number');
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('http://numbersapi.com/$number');
 
   @override
-  Future<NumberTriviaModel> getRandomNumberTrivia() => _getTriviaFromUrl('http://numbersapi.com/random');
-
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('http://numbersapi.com/random');
 
   Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
     final response = await client.get(
-      url,
+      Uri.https(url),
       headers: {'Content-Type': 'application/json'},
     );
 
---
+++ lib/injection_container.dart
@@ -0,0 +1,55 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:number_trivia/core/network/network_info.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_sourse.dart';
+import 'package:number_trivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance; // service locator
+
+Future<void> init() async {
+  // !Features - Number Trivia
+  // bloc
+  sl.registerFactory(() => NumberTriviaBloc(
+        // always obtain new instance on every call
+        concrete: sl(),
+        inputConverter: sl(),
+        random: sl(),
+      ));
+  // usecases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(
+      sl())); // same instance, lazy - only regirters when used
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+  // Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+    () => NumberTriviaRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+  // Data sources
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(
+    () => NumberTriviaRemoteDataSourceImpl(client: sl()),
+  );
+
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(
+    () => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()),
+  );
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
